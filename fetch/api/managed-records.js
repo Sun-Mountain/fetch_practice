ca3b1e5869bedc0d14050cb88706e1f2528147d8
Sun-Mountain <npzonnenberg@gmail.com>
2019-10-15 08:54:27 -0400

    PROGRESS

--- fetch/api/managed-records.js
+++ fetch/api/managed-records.js
@@ -1,29 +1,45 @@
 import fetch from "../util/fetch-fill";
 import URI from "urijs";
 
-// /records endpoint
+// records endpoint
 window.path = "http://localhost:3000/records";
 
 // Your retrieve function plus any additional functions go here ...
 
-function retrieve() {
+const retrieve = async function (options) {
 
     try {
+        const records = await fetch("http://localhost:3000/records");
+        const jsonRecords = await records.json();
 
-        fetch(window.path)
+        let result = {
+            ids: [],
+            open: [],
+            closedPrimaryCount: [],
+            previousPage: null,
+            nextPage: null
+        }
 
-    } catch (error) {
-        console.log("error");
-    };
+        if (options && options.page) {
+            const offset = (options.page - 1) * 10;
+            const items = jsonRecords.slice(offset, offset + 10);
+            console.log("items:", items);
+
+            result.previousPage = options.page - 1
+            result.nextPage = options.page + 1
+        } else {
+            result = jsonRecords.slice(0, 9);
+            console.log(result);
+        }
+
+        // console.log("JSON RESULTS:", jsonRecords);
+
+        return result;
+    }
+    catch (err) {
+        console.log(err);
+    }
 
 };
-
-function getUrlVars() {
-    var vars = {};
-    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function (m, key, value) {
-        vars[key] = value;
-    });
-    return vars;
-}
 
 export default retrieve;
