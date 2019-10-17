c100dda525fa0785952031b40724e1cac855a459
Sun-Mountain <npzonnenberg@gmail.com>
2019-10-17 06:02:36 -0400

    slowprogress

--- fetch/api/managed-records.js
+++ fetch/api/managed-records.js
@@ -8,29 +8,67 @@ window.path = "http://localhost:3000/records";
 
 const retrieve = async function (options) {
 
+    var uri = new URI({
+        protocol: "http",
+        hostname: "localhost:3000/records"
+    })
+
     try {
-        const records = await fetch("http://localhost:3000/records");
+        const records = await fetch(uri);
         const jsonRecords = await records.json();
 
-        let result = {
-            ids: [],
-            open: [],
-            closedPrimaryCount: [],
-            previousPage: null,
-            nextPage: null
-        }
+        let result = {}
 
-        if (options && options.page) {
-            const offset = (options.page - 1) * 10;
-            const items = jsonRecords.slice(offset, offset + 10);
-            console.log("items:", items);
+        console.log(jsonRecords);
 
-            result.previousPage = options.page - 1
-            result.nextPage = options.page + 1
+        const offset = (options.page - 1) * 10;
+        const items = jsonRecords.slice(offset, offset + 10);
+
+        items.map(item => {
+            if (item.color == "red" || item.color == "yellow" || item.color == "blue") {
+                item.isPrimary = true;
+            } else {
+                item.isPrimary = false;
+            }
+        })
+
+        const primaryClosed = items.filter(function (item) {
+            if (item.disposition === "closed" && item.isPrimary === true) {
+                return item;
+            }
+        });
+
+        if (options.page - 1 === 0) {
+            result.previousPage = null;
         } else {
-            result = jsonRecords.slice(0, 9);
-            console.log(result);
+            result.previousPage = options.page - 1;
         }
+
+        result.nextPage = options.page + 1;
+        result.ids = items.map(item => item.id);
+        result.open = items.filter(function (item) {
+            return item.disposition === "open";
+        });
+
+        // if (!options.colors) {
+        //     result.open = items.filter(function (item) {
+        //         return item.disposition === "open";
+        //     });
+        // } else {
+        //     const colors = options.colors;
+
+        //     result.open = items.filter(function (item) {
+        //         if (item.disposition === "open") {
+        //             colors.map(function (color) {
+        //                 return item.color === color;
+        //             })
+        //         }
+        //     });
+        // };
+
+        result.closedPrimaryCount = primaryClosed.length;
+
+        console.log("result:", result);
 
         // console.log("JSON RESULTS:", jsonRecords);
 
